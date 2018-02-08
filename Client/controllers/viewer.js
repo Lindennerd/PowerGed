b66a9391a6ff2c3a53cdc411a5251a5231e7c07d
Luiz Paulo <luiz.paulo@powerpatria.corp>
2018-02-08 11:12:13 -0200

    angular material

--- Client/controllers/viewer.js
+++ Client/controllers/viewer.js
@@ -8,33 +8,39 @@ angular.module("PowerGed")
         $('.button-collapse.side-nav-right').sideNav({edge: 'right'});
         $('[data-activates="file-viewer"]').sideNav({edge: 'right', draggable: true, menuWidth: 800});
 
-        $scope.treeOptions = {
-            nodeChildren: "children",
-            dirSelectable: true,
-            injectClasses: {
-                ul: "a1",
-                li: "a2",
-                liSelected: "a7",
-                iExpanded: "a3",
-                iCollapsed: "a4",
-                iLeaf: "a5",
-                label: "a6",
-                labelSelected: "a8"
-            }
-        }
-
-        $scope.dataForTheTree = [
-            { "name" : "Joe", "age" : "21", "children" : [
-                { "name" : "Smith", "age" : "42", "children" : [] },
-                { "name" : "Gary", "age" : "21", "children" : [
-                    { "name" : "Jenifer", "age" : "23", "children" : [
-                        { "name" : "Dani", "age" : "32", "children" : [] },
-                        { "name" : "Max", "age" : "34", "children" : [] }
-                    ]}
-                ]}
-            ]},
-            { "name" : "Albert", "age" : "33", "children" : [] },
-            { "name" : "Ron", "age" : "29", "children" : [] }
-        ];
         
     });
+
+angular.module("PowerGed").filter('getBasesName', function(){
+    return function(base){
+        return {name: base.name, id: base.id};
+    }
+})
+
+angular.module("PowerGed").controller('treeview', function($scope, $http){
+
+    $scope.treeOptions = {
+        nodeChildren: "children",
+        dirSelectable: false,
+        injectClasses: {
+            ul: "a1",
+            li: "a2",
+            liSelected: "a7",
+            iExpanded: "a3",
+            iCollapsed: "a4",
+            iLeaf: "a5",
+            label: "a6",
+            labelSelected: "a8"
+        }
+    }
+  
+
+    //$scope.dataForTheTree = response.data;
+
+    $http.get(config.urls.base + '/bases').then(function(response) {
+        $scope.bases = response.data;
+        $scope.basesName = response.data.map(function(element, index) {
+            return {name: element.name, id: element.id};
+        });
+    })
+});
--- Server/app.js
+++ Server/app.js
@@ -2,6 +2,9 @@ var bodyParser = require('body-parser');
 var express = require("express");
 var cors = require('cors');
 
+/* MOCK */
+var bases = require('./bases.json');
+
 var app = express();
 app.use(bodyParser.json())
 
@@ -13,6 +16,12 @@ app.post('/login', function(req,res,next){
     console.log(req.body);
     next();
 });
+
+app.get('/bases', function(req, res, next){
+    res.send(bases);
+
+    next();
+})
 
 app.listen('6969', function() {
     console.log('coco');
