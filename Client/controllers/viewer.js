2e3a16a0db863668f85b5932aa0c65de1b00b58e
Paulo Reis <paulo.reis@powerpatria.corp>
2018-02-08 14:42:51 -0200

    Construção do visualizador de arquivo e campos indexados.

--- Client/controllers/viewer.js
+++ Client/controllers/viewer.js
@@ -1,12 +1,13 @@
 angular.module("PowerGed")
     .controller('viewer', function($scope){
         $('.dropdown-button').dropdown();
+        $('.modal').modal({dismissible: false,opacity: 0, startingTop: '4%',endingTop: '10%',});
         $('select').material_select();
         $('.collapsible').collapsible();
-
-        $('.button-collapse').sideNav( {draggable: true} );
+        $('close-view').sideNav('hide');
+        $('.button-collapse').sideNav( {draggable: false} );
         $('.button-collapse.side-nav-right').sideNav({edge: 'right'});
-        $('[data-activates="file-viewer"]').sideNav({edge: 'right', draggable: true, menuWidth: 800});
+        $('[data-activates="file-viewer"]').sideNav({closeOnClick: true, edge: 'right', draggable: true, menuWidth: 700});
 
         $scope.treeOptions = {
             nodeChildren: "children",
