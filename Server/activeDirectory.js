8919f14f1952b45513e60ad56b2dc769d26885e0
Luiz Paulo <luiz.paulo@powerpatria.corp>
2018-04-17 14:12:26 -0300

    ad configurations moved

--- Server/activeDirectory.js
+++ Server/activeDirectory.js
@@ -1,17 +1,6 @@
 const activeDirectory = require('activedirectory');
+const config = require('./config');
 
-const config = {
-    // url: 'ldap://dc.domain.com',
-    // baseDN: 'dc=domain,dc=com',
-    // username: 'username@domain.com',
-    // password: 'password'
-
-    url: 'LDAP://powerpatria.corp',
-    baseDN: 'CN=Users,DC=domain',
-    username: 'POWERPATRIA\\localservices',
-    password: 'p1m4g1ng'
-}
-
-const ad = new activeDirectory(config);
+const ad = new activeDirectory(config.activeDirectory);
 
 module.exports = ad;
